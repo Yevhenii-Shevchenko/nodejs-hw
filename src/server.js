5166236759da30138bd9e1557e77904e50007c9c
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-05-07 22:18:48 +0300

    add :nouteId, error

--- src/server.js
+++ src/server.js
@@ -6,14 +6,34 @@ const message = "Hello world Evgeniy";
 
 console.log(message);
 
-app.get("/notes", (req, res) => {res.status(200).json({
-    "message": "Retrieved all notes",
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Retrieved all notes",
   });
 });
 
-// app.get("/notes". () => {console.log();
-// });
+app.get("/notes/:noteId", (req, res) => {
+  const { noteId } = req.params;
+   res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`
+  });
+});
 
+app.get("/test-error", () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message
+    // message: "Houston has broblem"
+  });
+ });
 
 app.listen(3000, () => { console.log('Server is running on port 3000');
 });
