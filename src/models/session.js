5b2d7d280d1af1a0aa829b016d9996e705f42454
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-06-02 18:45:34 +0300

    add sessionSchema

--- src/models/session.js
+++ src/models/session.js
@@ -1,37 +1,30 @@
 import { Schema, model } from 'mongoose';
 
-const sessionSchema = new Schema({
+const sessionSchema = new Schema(
+  {
     userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
     accessToken: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     refreshToken: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     accessTokenValidUntil: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     refreshTokenValidUntil: {
-        type: Date,
-        required: true
-    }
-}, { timestamps: true })
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-export const Session = model("Session", sessionSchema);
-
-
-У файлі src/models/session.js створіть модель Session із такими властивостями:
-
-userId — тип Schema.Types.ObjectId, обов’язкове, посилання на модель User;
-accessToken — рядок, обов’язкове;
-refreshToken — рядок, обов’язкове;
-accessTokenValidUntil — тип Date, обов’язкове;
-refreshTokenValidUntil — тип Date, обов’язкове.
-Для автоматичного створення полів createdAt та updatedAt, використовуйте параметр timestamps: true при створенні моделі.
+export const Session = model('Session', sessionSchema);
